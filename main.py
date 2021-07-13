09806627154eb5505751860252faec7e6b717569
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-07-13 16:43:26 -0700

    fixed bug where you couldn't lose

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ for i in range(enemyamount):
     enemy1img.append(pygame.image.load("enemy1.png"))
     enemy1x.append(random.randrange(65, 736))
     enemy1y.append(20)
-    enemy1xchange.append(.25)
+    enemy1xchange.append(.33)
     enemy1ychange.append(.01)
 
 missileimg = pygame.image.load("missile.png")
-missilex = 0
-missiley = 480
+missilex = 10
+missiley = 528
 missilexchange = 0
 missileychange = .4
 bullet_sate = "ready"
@@ -55,7 +55,7 @@ def enemy1(x,y, i):
 
 def missile(x,y):
     global bullet_sate
-    screen.blit(missileimg,(x + 16, y + 10))
+    screen.blit(missileimg,(x + 16, y))
     bullet_sate = "fire"
 
 def iscollision(enemyx, enemyy, missilex, missiley):
@@ -101,26 +101,27 @@ while running:
         enemy1y[i] += .01
         enemy1x[i] += enemy1xchange[i]
         if enemy1x[i] <= 0:
-            enemy1xchange[i] = .25
+            enemy1xchange[i] = .33
             enemy1x[i] += enemy1xchange[i]
         elif enemy1x[i] >= 736:
-            enemy1xchange[i] = -.25
+            enemy1xchange[i] = -.33
             enemy1y[i] += enemy1ychange[i]
+
+        if enemy1y[i] >= 500:
+            screen.blit(lost, (220, 100))
+            scoref(stextx, stexty)
+            pygame.display.update()
+            time.sleep(3)
+            break
 
         collision = iscollision(enemy1x[i], enemy1y[i], missilex, missiley)
 
         if collision:
-            missiley = 480
+            missiley = 528
             bullet_sate = "ready"
             score = score + 1
             enemy1x[i] = random.randint(0, 736)
             enemy1y[i] = random.randint(10, 30)
-
-        if enemy1y[i] >= 500:
-            screen.blit(lost, (220, 100))
-            pygame.display.update()
-            time.sleep(3)
-            break
 
     if bullet_sate == "fire":
         missile(missilex, missiley)
